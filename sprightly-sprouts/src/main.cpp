a5c0163ba74da240e3ec544622b652380cad6d8b
s184205 <s184205@student.dtu.dk>
2020-03-04 14:53:25 +0100

    started working on shaders

--- sprightly-sprouts/src/main.cpp
+++ sprightly-sprouts/src/main.cpp
@@ -1,18 +1,84 @@
+#include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include <iostream>
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = ourColor;\n"
+"}\n\0";
+
 
 int main() {
 	if (glfwInit() != GLFW_TRUE)
 		return 1;
 
-	GLFWwindow* window = glfwCreateWindow(100, 100, "Hello world", nullptr, nullptr);
-	if (!window) {
+	// Create new window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello world", nullptr, nullptr);
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
-		return 1;
+		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 
-	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+	// Load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Build and compile the shader program
+	// ------------------------------------
+	// Vertex shader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Check for errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Link shaders
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	while (glfwWindowShouldClose(window) != GL_TRUE) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
