d0cea83eff96e6105d145b8ab467a0644eae5a83
s184205 <s184205@student.dtu.dk>
2020-03-04 15:07:17 +0100

    updated version of OpenGL

--- sprightly-sprouts/src/main.cpp
+++ sprightly-sprouts/src/main.cpp
@@ -2,14 +2,14 @@
 #include<GLFW/glfw3.h>
 #include <iostream>
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 300 es\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
 "}\0";
 
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 300 es\n"
 "out vec4 FragColor;\n"
 "uniform vec4 ourColor;\n"
 "void main()\n"
