7262b042c8e6972ca651b0c166d5d470f9d4ad18
G4me4u <G4me4u@gmail.com>
2020-03-18 17:42:47 +0100

    Added math

---
+++ src/com/sprouts/math/LinMath.java
@@ -0,0 +1,7 @@
+package com.sprouts.math;
+
+public class LinMath {
+
+	public static final float EPSILON = 0.01f;
+	
+}
---
+++ src/com/sprouts/math/Vec4.java
@@ -0,0 +1,131 @@
+package com.sprouts.math;
+
+public class Vec4 {
+
+	public float x;
+	public float y;
+	public float z;
+	public float w;
+	
+	public Vec4(float c) {
+		this(c, c, c, c);
+	}
+
+	public Vec4(float x, float y, float z, float w) {
+		set(x, y, z, w);
+	}
+
+	public Vec4(Vec4 other) {
+		set(other);
+	}
+
+	public Vec4 set(Vec4 other) {
+		return set(other.x, other.y, other.z, other.w);
+	}
+
+	public Vec4 set(float c) {
+		return set(c, c, c, c);
+	}
+	
+	public Vec4 set(float x, float y, float z, float w) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+		
+		return this;
+	}
+
+	public Vec4 add(Vec4 other) {
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		w += other.w;
+
+		return this;
+	}
+
+	public Vec4 sub(Vec4 other) {
+		x -= other.x;
+		y -= other.y;
+		z -= other.z;
+		w -= other.w;
+
+		return this;
+	}
+
+	public Vec4 mul(Vec4 other) {
+		x *= other.x;
+		y *= other.y;
+		z *= other.z;
+
+		w *= other.w;
+
+		return this;
+	}
+
+	public Vec4 div(Vec4 other) {
+		x /= other.x;
+		y /= other.y;
+		z /= other.z;
+		w /= other.w;
+
+		return this;
+	}
+
+	public Vec4 add(float c) {
+		x += c;
+		y += c;
+		z += c;
+		w += c;
+
+		return this;
+	}
+
+	public Vec4 sub(float c) {
+		x -= c;
+		y -= c;
+		z -= c;
+		w -= c;
+
+		return this;
+	}
+
+	public Vec4 mul(float c) {
+		x *= c;
+		y *= c;
+		z *= c;
+		w *= c;
+
+		return this;
+	}
+
+	public Vec4 div(float c) {
+		x /= c;
+		y /= c;
+		z /= c;
+		w /= c;
+
+		return this;
+	}
+
+	public float dot(Vec4 other) {
+		return x * other.x + 
+		       y * other.y + 
+		       z * other.z + 
+		       w * other.w;
+	}
+
+	public float lengthSqr() {
+		return dot(this);
+	}
+
+	public float length() {
+		return (float)Math.sqrt(lengthSqr());
+	}
+
+	public Vec4 normalize() {
+		float len = length();
+		return (len < LinMath.EPSILON) ? set(0.0f) : div(len);
+	}
+}
