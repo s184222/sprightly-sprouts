3c53e178ba852a5e6ac9c8bff5de2d5a6b4037e3
s184222 <christianf.rtg@gmail.com>
2020-04-29 14:46:21 +0200

    Added FrameBufferType as a parameter to the frame buffer object

--- src/com/sprouts/graphic/buffer/FrameBuffer.java
+++ src/com/sprouts/graphic/buffer/FrameBuffer.java
@@ -1,59 +1,121 @@
 package com.sprouts.graphic.buffer;
 
-import java.nio.ByteBuffer;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_COMPONENT;
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
+import static org.lwjgl.opengl.GL11.GL_RGB;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glDrawBuffer;
+import static org.lwjgl.opengl.GL11.glGenTextures;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
+import static org.lwjgl.opengl.GL30.GL_COLOR_ATTACHMENT0;
+import static org.lwjgl.opengl.GL30.GL_DEPTH_ATTACHMENT;
+import static org.lwjgl.opengl.GL30.GL_FRAMEBUFFER;
+import static org.lwjgl.opengl.GL30.GL_RENDERBUFFER;
+import static org.lwjgl.opengl.GL30.glBindFramebuffer;
+import static org.lwjgl.opengl.GL30.glBindRenderbuffer;
+import static org.lwjgl.opengl.GL30.glFramebufferRenderbuffer;
+import static org.lwjgl.opengl.GL30.glGenFramebuffers;
+import static org.lwjgl.opengl.GL30.glGenRenderbuffers;
+import static org.lwjgl.opengl.GL30.glRenderbufferStorage;
+import static org.lwjgl.opengl.GL32.glFramebufferTexture;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL30;
-import org.lwjgl.opengl.GL32;
+import java.nio.ByteBuffer;
 
 public class FrameBuffer {
 	
+	private final FrameBufferType type;
+	private int width;
+	private int height;
 	
-	public int frameBufferID;
-	public int textureID;
-	public int depthBufferID;
-	
-	public FrameBuffer() {
-		initializeFrameBuffer();
+	private final int frameBufferID;
+	private final int textureID;
+	private final int depthBufferID;
+
+	public FrameBuffer(FrameBufferType type, int width, int height) {
+		this.type = type;
+		this.width = width;
+		this.height = height;
+		
+		frameBufferID = glGenFramebuffers();
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, frameBufferID);
+		textureID = type.hasTextureAttachment() ? createTextureAttachment() : -1;
+		depthBufferID = type.hasDepthAttachment() ? createDepthAttachment() : -1;
+
+		updateAttachmentSize();
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
 
-	private void initializeFrameBuffer() {
-		frameBufferID = GL30.glGenFramebuffers();
-		bindFrameBuffer();
-		GL11.glDrawBuffer(GL30.GL_COLOR_ATTACHMENT0);
-		unbindCurrentFrameBuffer();
+	private static int createTextureAttachment() {
+		int textureID = glGenTextures();
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, textureID, 0);
+		glDrawBuffer(GL_COLOR_ATTACHMENT0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		return textureID;
 	}
 	
-	public void bindFrameBuffer(){
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);//To make sure the texture isn't bound
-		GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, frameBufferID);
-	}
-	
-	public void unbindCurrentFrameBuffer() {
-		GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
-	}
-	
-	public void createTextureAttachment(int width, int height) {
-		bindFrameBuffer();
-		textureID = GL11.glGenTextures();
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
-		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, width, height,
-				0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, (ByteBuffer) null);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-		GL32.glFramebufferTexture(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0,
-				textureID, 0);
-		unbindCurrentFrameBuffer();
+	private static int createDepthAttachment() {
+		int depthBufferID = glGenRenderbuffers();
+		glBindRenderbuffer(GL_RENDERBUFFER, depthBufferID);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBufferID);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		return depthBufferID;
 	}
 
-	public void createDepthBufferAttachment(int width, int height) {
-		bindFrameBuffer();
-		depthBufferID = GL30.glGenRenderbuffers();
-		GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, depthBufferID);
-		GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL11.GL_DEPTH_COMPONENT, width,
-				height);
-		GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT,
-				GL30.GL_RENDERBUFFER, depthBufferID);
-		unbindCurrentFrameBuffer();
+	private void updateAttachmentSize() {
+		if (type.hasTextureAttachment()) {
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (ByteBuffer)null);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+		
+		if (type.hasDepthAttachment()) {
+			glBindRenderbuffer(GL_RENDERBUFFER, depthBufferID);
+			glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+			glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		}
+	}
+	
+	public void bind(){
+		glBindFramebuffer(GL_FRAMEBUFFER, frameBufferID);
+	}
+	
+	public void unbind() {
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+	
+	public void setSize(int width, int height) {
+		if (width <= 0 || height <= 0)
+			throw new IllegalArgumentException("Dimensions must be positive.");
+		
+		if (this.width != width || this.height != height) {
+			this.width = width;
+			this.height = height;
+			
+			bind();
+			updateAttachmentSize();
+			unbind();
+		}
+	}
+
+	public FrameBufferType getType() {
+		return type;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 }
