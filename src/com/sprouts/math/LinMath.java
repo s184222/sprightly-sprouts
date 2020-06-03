0b5246b56728c8ea16ef2a78c0a1ebf83bd92190
G4me4u <G4me4u@gmail.com>
2020-06-03 16:27:04 +0200

    Added clamp functions to LinMath class

--- src/com/sprouts/math/LinMath.java
+++ src/com/sprouts/math/LinMath.java
@@ -6,4 +6,20 @@ public final class LinMath {
 	
 	private LinMath() {
 	}
+	
+	public static int clamp(int v, int mn, int mx) {
+		return v < mn ? mn : (v > mx ? mx : v);
+	}
+
+	public static float clamp(float v, float mn, float mx) {
+		return v < mn ? mn : (v > mx ? mx : v);
+	}
+
+	public static long clamp(long v, long mn, long mx) {
+		return v < mn ? mn : (v > mx ? mx : v);
+	}
+	
+	public static double clamp(double v, double mn, double mx) {
+		return v < mn ? mn : (v > mx ? mx : v);
+	}
 }
