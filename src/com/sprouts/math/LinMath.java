f849a9fe0fd7b257bfff935b6a69b3d78247d6a7
Tobias Holtoft <s184217@student.dtu.dk>
2020-06-11 14:34:28 +0200

    improvements on intersection and contains

--- src/com/sprouts/math/LinMath.java
+++ src/com/sprouts/math/LinMath.java
@@ -1,5 +1,6 @@
 package com.sprouts.math;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class LinMath {
@@ -45,33 +46,79 @@ public final class LinMath {
 		return intersect(p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, b, c);
 	}
 
-	public static Vec2 intersect(float p0x, float p0y, float p1x, float p1y, float p2x, float p2y, float p3x, float p3y, boolean b, boolean c) {
-		float a1 = p1y - p0y;
-		float b1 = p0x - p1x;
-		float c1 = a1 * p1x + b1 * p1y;
-		float a2 = p3y - p2y;
-		float b2 = p2x - p3x;
-		float c2 = a2 * p2x + b2 * p2y;
-		float divisor = a1 * b2 - a2 * b1;
+	public static Vec2 intersect(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, boolean startOpen, boolean endOpen) {
+		double a1 = y1 - y0;
+		double b1 = x0 - x1;
+		double c1 = a1 * x1 + b1 * y1;
+		double a2 = y3 - y2;
+		double b2 = x2 - x3;
+		double c2 = a2 * x2 + b2 * y2;
+		double divisor = a1 * b2 - a2 * b1;
 
 		if (Math.abs(divisor) < EPSILON * EPSILON) {
 			return null; // if divide == 0 it means the lines are parallel
 		}
 
-		float x = (b2 * c1 - b1 * c2) / divisor;
-		float y = (a1 * c2 - a2 * c1) / divisor;
-		float ratiox1 = (x - p0x) / (p1x - p0x);
-		float ratioy1 = (y - p0y) / (p1y - p0y);
-		float ratiox2 = (x - p2x) / (p3x - p2x);
-		float ratioy2 = (y - p2y) / (p3y - p2y);
-
-		if (((ratiox1 >= 0 && ratiox1 <= 1) || (ratioy1 >= 0 && ratioy1 <= 1)) && ((ratiox2 >= 0 && ratiox2 <= 1) || (ratioy2 >= 0 && ratioy2 <= 1))) {
-			return new Vec2(x, y);
+		double x = (b2 * c1 - b1 * c2) / divisor;
+		double y = (a1 * c2 - a2 * c1) / divisor;
+		double ratiox1 = (x - x0) / (x1 - x0);
+		double ratioy1 = (y - y0) / (y1 - y0);
+		double ratiox2 = (x - x2) / (x3 - x2);
+		double ratioy2 = (y - y2) / (y3 - y2);
+		System.out.println(ratiox1 + " " + ratioy1 + " " + ratiox2 + " " + ratioy2 + " " + x + " " + y + " " + x1 + " " + y1 + " " + EPSILON);
+		if (startOpen) {
+			if (endOpen) {
+				if (((ratiox1 > 0-EPSILON && ratiox1 < 1+EPSILON) || (ratioy1 > 0-EPSILON && ratioy1 < 1+EPSILON)) && 
+						((ratiox2 >= 0-EPSILON && ratiox2 <= 1+EPSILON) || (ratioy2 >= 0-EPSILON && ratioy2 <= 1+EPSILON))){
+					return new Vec2((float)x, (float)y);
+				}
+			} else if (((ratiox1 > 0-EPSILON && ratiox1 <= 1+EPSILON) || (ratioy1 > 0-EPSILON && ratioy1 <= 1+EPSILON)) && 
+					((ratiox2 >= 0-EPSILON && ratiox2 <= 1+EPSILON) || (ratioy2 >= 0-EPSILON && ratioy2 <= 1+EPSILON))){
+				return new Vec2((float)x, (float)y);
+			}
+		} else if (endOpen) {
+			if (((ratiox1 >= 0-EPSILON && ratiox1 < 1+EPSILON) || (ratioy1 >= 0-EPSILON && ratioy1 < 1+EPSILON)) && 
+					((ratiox2 >= 0-EPSILON && ratiox2 <= 1+EPSILON) || (ratioy2 >= 0-EPSILON&& ratioy2 <= 1+EPSILON))){
+				return new Vec2((float)x, (float)y);
+			}	
+		} else if (((ratiox1 >= 0-EPSILON && ratiox1 <= 1+EPSILON) || (ratioy1 >= 0-EPSILON && ratioy1 <= 1+EPSILON)) && 
+				((ratiox2 >= 0-EPSILON && ratiox2 <= 1+EPSILON) || (ratioy2 >= 0-EPSILON && ratioy2 <= 1+EPSILON))){
+			return new Vec2((float)x, (float)y);
 		}
 		
 		return null;
 	}
-
+	//this intersect is only used for the function contains 
+	public static Vec2 intersect(Vec2 p0, Vec2 p1, Vec2 p2, Vec2 p3) {
+		float a1 = p1.y - p0.y;
+		float b1 = p0.x - p1.x;
+		float c1 = a1 * p1.x + b1 * p1.y;
+		float a2 = p3.y - p2.y;
+		float b2 =p2.x - p3.x;
+		float c2 =a2 * p2.x + b2 * p2.y;
+		float divisor = a1 * b2 - a2 * b1;
+		
+		if (Math.abs(divisor) < EPSILON*EPSILON) {
+			return null; // if divide == 0 it means the lines are parallel
+		}
+		
+		float x = (b2*c1-b1*c2)/divisor;
+		float y = (a1*c2-a2*c1)/divisor;
+		
+		//if x and y is the same as the point we want to check, return the vec2, so that we can catch it in the function Contains
+		if (x==p2.x && y==p2.y) return new Vec2(x, y); 
+		
+		float ratiox1 = (x - p0.x) / (p1.x - p0.x); //if p1x == p0x then ratiox1 will be NaN
+		float ratioy1 = (y - p0.y) / (p1.y - p0.y); //if p1y == p0y then ratioy1 will be NaN
+		float ratiox2 = (x - p2.x) / (p3.x - p2.x); //if p3x == p2x then ratiox2 will be NaN
+		float ratioy2 = (y - p2.y) / (p3.y - p2.y); //if p3y == p2y then ratioy2 will be NaN
+		if (((ratiox1 > 0 && ratiox1 <=1) || (ratioy1 > 0 && ratioy1 <= 1)) && 
+				((ratiox2 > 0 && ratiox2 <=1) || (ratioy2 > 0 && ratioy2 <= 1))){
+			
+			return new Vec2(x, y);
+		}
+		return null;
+	}
 	/* for test:
 	 * public static void main(String[] args) {
 	 *     Point p0 = new Point (2,3);
@@ -85,14 +132,16 @@ public final class LinMath {
 	 * source: https://www.youtube.com/watch?v=HYAgJN3x4GA&t=164s
 	 */
 	public static boolean contains(List<Vec2> poly, Vec2 v) {
-		Vec2 vNew = new Vec2(v.x + 10000, v.y);
+		Vec2 vNew = new Vec2(v.x + 10, v.y + 10);
 		
 		int increment = 0;
 		for (int i = 0; i < poly.size() - 1; i++) {
 			Vec2 p = intersect(poly.get(i), poly.get(i + 1), v, vNew, true, true);
 			
-			if (p != null)
+			if (p != null) {
+				if (p.x==v.x && p.y==v.y) return false;
 				increment++;
+			}
 		}
 		
 		return (increment % 2 != 0);
@@ -114,5 +163,19 @@ public final class LinMath {
 		float w2 = -((bx * py) - (by * px)) / divisor;
 
 		return (w1 >= 0.0f && w2 >= 0.0f && (w1 + w2) <= 1.0f);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		List<Vec2> list = new ArrayList<Vec2>();
+		list.add(new Vec2(0,0));
+		list.add(new Vec2(3,2));
+		list.add(new Vec2(4,2));
+		list.add(new Vec2(2,6));
+		list.add(new Vec2(5,4));
+		list.add(new Vec2(5,1));
+//		System.out.println(LinMath.contains(list,new Vec2(4,2)));
+		System.out.println(intersect(new Vec2(0,0),new Vec2(3,3),new Vec2(2,3),new Vec2(4,3),false,true));
 	}
 }
